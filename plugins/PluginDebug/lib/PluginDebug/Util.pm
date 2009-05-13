b0e0cedabc775126d41cf03b6cf33c3163d2f01f
Steve <sivy@fairbairn-sykes-2.local>
2009-05-13 14:23:48 -0700

    make debuglog a method on Component, not Plugin (this means that both plugins and addons can make use of it).

--- plugins/PluginDebug/lib/PluginDebug/Util.pm
+++ plugins/PluginDebug/lib/PluginDebug/Util.pm
@@ -4,14 +4,14 @@ use strict;
 use warnings;
 
 sub init {
-    require MT::Plugin;
-    *MT::Plugin::debuglog = \&debuglog;
+    require MT::Component;
+    *MT::Component::debuglog = \&debuglog;
 }
 
 sub debuglog {
     return unless MT->config->PluginDebug;
-    my ( $plugin, $msg ) = @_;
-    my $id = $plugin->id;
+    my ( $component, $msg ) = @_;
+    my $id = $component->id;
     require MT::Log;
     if (   MT->config->PluginDebug eq 'all'
         || MT->config->PluginDebug =~ /$id/i )
@@ -19,7 +19,7 @@ sub debuglog {
         MT->log(
             {   message => "$id: $msg",
                 level   => MT::Log::DEBUG(),
-                category => 'plugin_debug'
+                category => 'component_debug'
             }
         );
     }
